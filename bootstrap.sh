8fd4d8f1f0fee0dd0ca2cef4ae7837491affa244
Sam Edwards <sam@thenumber.ca>
2016-12-20 16:49:17 -0800

    Add sd for quick directory changing

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,12 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 git submodule update --init
+cp sd/sd .sd
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "firstrun.sh" \
-		--exclude "brew.sh" --exclude "brew_cask.sh" -avh --no-perms . ~;
+		--exclude "brew.sh" --exclude "brew_cask.sh" --exclude "sd/" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
